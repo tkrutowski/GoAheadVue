03e3bc5d41f5bb8e8f6c25b4fa66bccde340b64c
Tomasz Krutowski <tkrutowski@gmail.com>
2023-11-01 22:58:45 +0100

    switch to prod

--- src/http-common.ts
+++ src/http-common.ts
@@ -1,8 +1,8 @@
 import axios, { AxiosInstance } from "axios";
 
 const apiClient: AxiosInstance = axios.create({
-  // baseURL: "https://goahead.focikhome.synology.me/api",
-  baseURL: "http://localhost:8077/api",
+  baseURL: "https://goahead.focikhome.synology.me/api",
+  // baseURL: "http://localhost:8077/api",
   headers: {
     "Content-type": "application/json",
   },
